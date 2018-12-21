b1ab6aae0822060093195f96249b0195c5c29af7
eirik@nlink.no <eirik@nlink.no>
2018-12-22 00:07:38 +0100

    I think it worksgit add .

--- src/scanner.cpp
+++ src/scanner.cpp
@@ -9,16 +9,20 @@ using namespace cv;
 
 ros::NodeHandle * nh;
 Mat image;
-int rl = 110;
-int gl = 30;
-int bl = 30;
+int rl = 180;
+int gl = 0;
+int bl = 20;
 
-int rh = 150;
+int rh = 256;
 int gh = 95;
-int bh = 95;
+int bh = 130;
 
 double alpha;
 double beta;
+
+
+std::vector<double > depths;
+
 
 Mat src1;
 Mat src2;
@@ -49,11 +53,12 @@ pcl::PointCloud<pcl::PointXYZRGB>::Ptr img_to_cloud(
         const cv::Mat &coords)
 {
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>());
+    std::cout << "size: " << depths.size() << std::endl;
+    std::cout << "sizer: " << image.rows << std::endl;
+    std::cout << "sizec: " << image.cols << std::endl;
 
-    for (int y=0;y<image.rows;y++)
-    {
-        for (int x=0;x<image.cols;x++)
-        {
+    for (int y=0;y<image.rows;y++){
+        for (int x=0;x<image.cols;x++){
             pcl::PointXYZRGB point;
 
 
@@ -62,28 +67,32 @@ pcl::PointCloud<pcl::PointXYZRGB>::Ptr img_to_cloud(
             int g = (color[1]);
             int b = (color[0]);
 
-            if(g == 0) {
+
                 point.x = coords.at<double>(0,y*image.cols+x);
                 point.y = coords.at<double>(1,y*image.cols+x);
-                point.z = coords.at<double>(2,y*image.cols+x) + 1;
-            }else {
-                point.x = coords.at<double>(0,y*image.cols+x);
-                point.y = coords.at<double>(1,y*image.cols+x);
-                point.z = coords.at<double>(2,y*image.cols+x);
-            }
+                point.z = depths[y];
+
 
             int32_t rgb = (r << 16) | (g << 8) | b;
             point.rgb = *reinterpret_cast<float*>(&rgb);
+
             if((r+g+b)!= 0){
                 cloud->points.push_back(point);
             }
+
+
 
         }
     }
     return cloud;
 }
 
-
+std::vector<cv::Vec4i> extractAllLines(const cv::Mat& image, int threshold, double minLength = 100)
+{
+    std::vector<cv::Vec4i> lines;
+    cv::HoughLinesP(image, lines, 1, CV_PI / 180, threshold, minLength);
+    return lines;
+}
 
 
 int main(int argc, char **argv) {
@@ -91,14 +100,13 @@ int main(int argc, char **argv) {
     ros::init(argc, argv, "nscan");
     nh = new ros::NodeHandle("nscan");
 
-    namedWindow( window_name, WINDOW_NORMAL );
-    cv::resizeWindow(window_name, 1000, 1000);
+    //namedWindow( window_name, WINDOW_NORMAL );
+    //cv::resizeWindow(window_name, 1000, 1000);
 
     src = imread(argv[1], CV_LOAD_IMAGE_COLOR);   // Read the file
-    cv::resize(src, src, cv::Size(src.rows,src.rows), 0, 0, CV_INTER_LINEAR);
 
     image = src;
-    image.convertTo(image, -1, 0.5, 0);
+
     if(! image.data )                              // Check for invalid input
     {
         cout <<  "Could not open or find the image" << std::endl ;
@@ -132,42 +140,42 @@ int main(int argc, char **argv) {
     cv::Mat lower_red_hue_range;
     cv::Mat upper_red_hue_range;
     cv::inRange(image, cv::Scalar(bl, gl, rl), cv::Scalar(bh, gh, rh), lower_red_hue_range);
-
-
-
-
-
+    int pixel;
+    for (int x=0;x<lower_red_hue_range.rows;x++){
+        for (int y=0;y<lower_red_hue_range.cols;y++){
+           pixel = (int)lower_red_hue_range.at<uchar>(x, y);
+           if(pixel){
+               double depth = (y-lower_red_hue_range.cols/2)*0.005 + 1.1942;
+               std::cout << "Found at col: " <<depth  << std::endl;
+               depths.push_back(depth);
+               break;
+           }
+           if(y==lower_red_hue_range.cols-2) {depths.push_back(-1);}
+        }
+    }
 
 
     Mat img;
+    Mat cloud_depth = lower_red_hue_range;
     cv::cvtColor(lower_red_hue_range, img, cv::COLOR_GRAY2BGR);
     int center_x = img.cols/2;
     int center_y = img.rows/2;
-    int angle = 500;
-    int bar_length = 600;
+    int angle = 0;
+    int bar_length = img.rows;
     line(img, Point(center_x+angle, center_y+bar_length),
          Point(center_x-angle, center_y-bar_length),
-         Scalar(255, 0, 255), 5);
+         Scalar(50, 0, 100), 5);
 
-    line(img, Point(center_x+bar_length, center_y-angle),
-         Point(center_x-bar_length, center_y+angle),
-         Scalar(255, 0, 255), 5);
+
 
     Mat lines_image = img;
-    img.setTo(cv::Scalar(0,255,255), img);
+    img.setTo(cv::Scalar(255,50,0), img);
 
-    src -= img;
-    int filter_size = 30;
-    int dividers = 10;
 
-    vector<float> filter_mask;
 
-    for (int i = 0;i < filter_size; i++){
-        if(i < filter_size/2)
-            filter_mask.push_back(i/10);
-        else
-            filter_mask.push_back(10-i/10);
-    }
+
+    src += img;
+
 
 
     for(int r = 0; r < lines_image.rows; r++){
@@ -179,15 +187,16 @@ int main(int argc, char **argv) {
     }
 
 
-    imshow(window_name, src);
+    //imshow(window_name, src);
 
 
 
-    cv::waitKey(0);
+    //cv::waitKey(0);
 
 
 
-    cv::Mat cloud_image = img;
+    cv::Mat cloud_image;
+    cv::cvtColor(lower_red_hue_range, cloud_image, cv::COLOR_GRAY2BGR);
 
     cv::Mat coords(3, cloud_image.cols * cloud_image.rows, CV_64FC1);
     for (int col = 0; col < coords.cols; ++col)
@@ -214,6 +223,8 @@ int main(int argc, char **argv) {
         ros::Duration(5).sleep();
         ros::spinOnce();
     }
+
+
 
 
     delete nh;
