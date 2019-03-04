251299af942ff2e24d43f4f03bdf8aa6d900f4b2
eirik@nlink.no <eirik@nlink.no>
2019-03-04 21:30:36 +0100

    feat: Interface and stearing with steppers

--- src/scanner.cpp
+++ src/scanner.cpp
@@ -10,11 +10,13 @@ using namespace cv;
 
 #include <tf2_ros/static_transform_broadcaster.h>
 #include <geometry_msgs/TransformStamped.h>
+#include <std_msgs/Float32.h>
+
 #include <cstdio>
 #include <tf2/LinearMath/Quaternion.h>
+#include <std_msgs/Empty.h>
 
-
-
+double g_distance = 1;
 double laser_angle;
 
 ros::NodeHandle * nh;
@@ -38,7 +40,7 @@ Mat filtered;
 Mat src1;
 Mat src2;
 string window_name = "katt";
-
+ros::Publisher chatter_pub;
 //Wht does line classifiers need
 int min_num_pixels;
 int max_num_pixels;
@@ -82,9 +84,10 @@ pcl::PointCloud<pcl::PointXYZRGB>::Ptr img_to_cloud(
         const cv::Mat &coords)
 {
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>());
-    //std::cout << "size: " << depths.size() << std::endl;
-    //std::cout << "sizer: " << image.rows << std::endl;
-    //std::cout << "sizec: " << image.cols << std::endl;
+    std::cout << "size: " << depths.size() << std::endl;
+    std::cout << "sizer: " << image.rows << std::endl;
+    std::cout << "sizec: " << image.cols << std::endl;
+    double l_distance = g_distance;
 
     for (int y=0;y<image.rows;y++){
         for (int x=0;x<image.cols;x++){
@@ -97,9 +100,9 @@ pcl::PointCloud<pcl::PointXYZRGB>::Ptr img_to_cloud(
             int b = (color[0]);
 
 
-            point.x = coords.at<double>(0,y*image.cols+x);
-            point.y = coords.at<double>(1,y*image.cols+x);
-            point.z = 1;
+            point.x = coords.at<double>(0,y*image.cols+x)-(image.cols/2.0)/5000.0;
+            point.z = coords.at<double>(1,y*image.cols+x)-(image.rows/2.0)/5000.0;
+            point.y = l_distance;
 
 
             int32_t rgb = (r << 16) | (g << 8) | b;
@@ -107,10 +110,6 @@ pcl::PointCloud<pcl::PointXYZRGB>::Ptr img_to_cloud(
 
 
             cloud->points.push_back(point);
-
-
-
-
 
 
         }
@@ -140,7 +139,7 @@ struct Operator
 
 
 void video_test(){
-    VideoCapture cap(0);
+    VideoCapture cap(1);
 
   // Check if camera opened successfully
   if(!cap.isOpened()){
@@ -148,71 +147,93 @@ void video_test(){
     return ;
   }
 
-  double expo = 0.25;
-  std::cout << "MJPG: " << cap.set(cv::CAP_PROP_FOURCC, cv::VideoWriter::fourcc('M', 'J', 'P', 'G'));
+  std::cout << "MJPG: " << cap.set(cv::CAP_PROP_FOURCC, cv::VideoWriter::fourcc('M', 'J', 'P', 'G'))  << std::endl;
   cap.set(CV_CAP_PROP_FRAME_WIDTH,640);
   cap.set(CV_CAP_PROP_FRAME_HEIGHT,480);
-  cap.set(CV_CAP_PROP_FPS, 10);
-  cap.set(CV_CAP_PROP_AUTO_EXPOSURE, false);
-  cap.set(CV_CAP_PROP_EXPOSURE, 500);
-  cap.set(CV_CAP_PROP_GAIN, 0);
+  cap.set(CV_CAP_PROP_FPS, 120);
+  //cap.set(CV_CAP_PROP_AUTO_EXPOSURE, true);
+  //cap.set(CV_CAP_PROP_EXPOSURE, 1500);
   double last_time = ros::Time::now().toSec();
   double now = last_time;
   Mat frame;
-  while(ros::ok()){
+	Mat gray;	
 
+	Mat final;
+  std_msgs::Empty msg;
+  Mat last_frame;
+  Mat frame_color;
+  Mat subtracted;
+
+    cap >> frame;
+    cap >> last_frame;
+    cap >> subtracted;
+
+
+  while(ros::ok()){
+      frame.copyTo(last_frame);
       cap >> frame;
       if (frame.empty())
           break;
 
-
+      absdiff(last_frame,frame, subtracted);
+      subtracted*=6.0;
+      chatter_pub.publish(msg);
+      ros::Duration(0.1).sleep();
       now = ros::Time::now().toSec();
       double time_duratinon = 1.0/(now-last_time);
       //double num_pixels = frame.rows*frame.cols;
-      //ROS_INFO("FPS: %f", time_duratinon);
+      ROS_INFO("FPS: %f", time_duratinon);
 
       last_time = now;
+
+	/*
       int ro= frame.rows/2;
       int co = frame.cols/2;
       cv::Vec3b color = frame.at<cv::Vec3b>(cv::Point(co,ro));
       int katt = color[0]+color[1]+color[2];
       ROS_INFO_STREAM(katt);
+      ROS_INFO_STREAM(time_duratinon);
+	*/
 
-    // Capture frame-by-frame
+      if (waitKey(5) >= 0)
+          break;
 
+      //convertScaleAbs(subtracted, subtracted);
 
-    // If the frame is empty, break immediately
+        cvtColor( subtracted, gray, CV_BGR2GRAY );
 
-    // Display the resulting frame
+      threshold( gray, gray, 100, 255,THRESH_BINARY );
+	 GaussianBlur( gray, gray, Size( 31, 31 ), 0, 0 );
+	cv::cvtColor(gray, gray, cv::COLOR_GRAY2RGB);
 
-    // Press  ESC on keyboard to exit
+	bitwise_and(frame, gray, final);
+
+    imshow(window_name, frame);
+
 
     int *i = 0;
 
-    //frame.forEach<Pixel>(Operator());
-    imshow(window_name, frame);
-    if (waitKey(5) >= 0)
-        break;
 
 
-      cv::Mat cloud_image = frame;
-      //cv::cvtColor(frame, cloud_image, cv::COLOR_GRAY2BGR);
+      cv::Mat cloud_image;
+      frame.copyTo(cloud_image);
 
       cv::Mat coords(3, cloud_image.cols * cloud_image.rows, CV_64FC1);
       for (int col = 0; col < coords.cols; ++col) {
-          coords.at<double>(0, col) = ((col % cloud_image.cols)) / 1000.0;
-          coords.at<double>(1, col) = ((col / cloud_image.cols)) / 1000.0;
-          coords.at<double>(2, col) = 2;
+          coords.at<double>(0, col) = ((col % cloud_image.cols)) / 5000.0;
+          coords.at<double>(1, col) = ((col / cloud_image.cols)) / 5000.0;
+          coords.at<double>(2, col) = 1;
       }
+
 
       pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud = img_to_cloud(cloud_image, coords);
 
 
       sensor_msgs::PointCloud2 trump;
       pcl::toROSMsg(*cloud, trump);
-      trump.header.frame_id = "laser";
-
-
+      trump.header.frame_id = "camera";
+      ROS_INFO("Publishing cloud with %i %i %i", trump.height, trump.width, trump.point_step);
+      ros::spinOnce();
       cloudpup.publish(trump);
 
     }
@@ -228,56 +249,24 @@ void video_test(){
   destroyAllWindows();
 
 }
-void init_transforms(){
 
-    static tf2_ros::StaticTransformBroadcaster static_broadcaster;
-    geometry_msgs::TransformStamped world_camera;
-
-    world_camera.header.stamp = ros::Time(0);
-    world_camera.header.frame_id = "world";
-    world_camera.child_frame_id = "camera";
-    world_camera.transform.translation.x = 0;
-    world_camera.transform.translation.y = 0;
-    world_camera.transform.translation.z = 1;
-    tf2::Quaternion quat;
-    quat.setRPY(0, 0, 0);
-    world_camera.transform.rotation.x = quat.x();
-    world_camera.transform.rotation.y = quat.y();
-    world_camera.transform.rotation.z = quat.z();
-    world_camera.transform.rotation.w = quat.w();
-
-
-
-    geometry_msgs::TransformStamped camera_laser;
-
-    camera_laser.header.stamp = ros::Time(0);
-    camera_laser.header.frame_id = "camera";
-    camera_laser.child_frame_id = "laser";
-    camera_laser.transform.translation.x = 0.6;
-    camera_laser.transform.translation.y = 0;
-    camera_laser.transform.translation.z = 0;
-    quat.setRPY(-1.4, 0, 0.3);
-    camera_laser.transform.rotation.x = quat.x();
-    camera_laser.transform.rotation.y = quat.y();
-    camera_laser.transform.rotation.z = quat.z();
-    camera_laser.transform.rotation.w = quat.w();
-    static_broadcaster.sendTransform(camera_laser);
-
-
-    static_broadcaster.sendTransform(world_camera);
-
+void distanceCB(const std_msgs::Float32::ConstPtr& msg)
+{
+    g_distance = msg->data;
+    ROS_INFO("Got distance form sfjhgfjukhg");
 }
 
 int main(int argc, char **argv) {
 
     ros::init(argc, argv, "nscan");
     nh = new ros::NodeHandle("nscan");
-    init_transforms();
     namedWindow( window_name, WINDOW_NORMAL );
     cv::resizeWindow(window_name, 1000, 1000);
+    chatter_pub = nh->advertise<std_msgs::Empty>("toggle_led", 0, true);
 
     start = ros::Time::now().toSec();
     cloudpup = nh->advertise<sensor_msgs::PointCloud2>("scan", 0);
+    ros::Subscriber distance_sub = nh->subscribe<std_msgs::Float32>("/distance_sensor0/distance",5, distanceCB);
 
     video_test();
     return 0;
